8b8717c9f548353a766029fb13929855fd97d366
Harshana Madusanka Jayamaha <jmhmjayamaha@gmail.com>
2017-10-29 21:55:28 +0530

    change the html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9zcSSYse_5X7HQl9fPQHIR20rPN-3MkI",
+    authDomain: "chatapp-45e1b.firebaseapp.com",
+    databaseURL: "https://chatapp-45e1b.firebaseio.com",
+    projectId: "chatapp-45e1b",
+    storageBucket: "",
+    messagingSenderId: "900541506712"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
